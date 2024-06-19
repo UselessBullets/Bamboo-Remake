510895ea3b9897611bd4eca4500fa12ef3daf79e
UselessBullets <80850784+UselessBullets@users.noreply.github.com>
2024-06-19 11:22:51 -0500

    slabs

--- src/main/java/mayonaka8478/bambooremake/block/SlabTatami.java
+++ src/main/java/mayonaka8478/bambooremake/block/SlabTatami.java
@@ -7,7 +7,9 @@ import net.minecraft.core.enums.PlacementMode;
 import net.minecraft.core.util.helper.Axis;
 import net.minecraft.core.util.helper.Direction;
 import net.minecraft.core.util.helper.Side;
+import net.minecraft.core.util.phys.AABB;
 import net.minecraft.core.world.World;
+import net.minecraft.core.world.WorldSource;
 
 public class SlabTatami extends BlockSlab {
 	public SlabTatami(Block modelBlock, int id) {
@@ -16,18 +18,65 @@ public class SlabTatami extends BlockSlab {
 
 	@Override
 	public void onBlockPlaced(World world, int x, int y, int z, Side side, EntityLiving entity, double sideHeight) {
-		Axis axis = entity.getPlacementDirection(side, PlacementMode.FACING).getAxis();
-		Direction dir = entity.getVerticalPlacementDirection(side, sideHeight);
-		int meta = world.getBlockMetadata(x, y, z) & 6;
-		if (dir == Direction.DOWN) {
-			meta = 0;
+		// Side is the side of the block you placed on, so if you place on the top of a block Side will equal SIDE.TOP
+		world.setBlockMetadataWithNotify(x, y, z, side.getOpposite().getId());
+	}
+
+	@Override
+	public void setBlockBoundsBasedOnState(WorldSource world, int x, int y, int z) {
+		int meta = world.getBlockMetadata(x, y, z);
+		boolean isDoubled = (meta & 0b1000) != 0;
+		if (isDoubled){
+			this.setBlockBounds(0.0, 0.0, 0.0, 1.0, 1.0, 1.0);
+			return;
 		}
-		if (dir == Direction.UP) {
-			meta = 2;
+
+		Side side = Side.getSideById(meta & 0b111);
+		switch (side){
+			case BOTTOM:
+				this.setBlockBounds(0.0, 0.0, 0.0, 1.0, 0.5, 1.0);
+				break;
+			case TOP:
+				this.setBlockBounds(0.0, 0.5, 0.0, 1.0, 1.0, 1.0);
+				break;
+			case NORTH:
+				this.setBlockBounds(0.0, 0.0, 0.0, 1.0, 1.0, 0.5);
+				break;
+			case SOUTH:
+				this.setBlockBounds(0.0, 0.0, 0.5, 1.0, 1.0, 1.0);
+				break;
+			case WEST:
+				this.setBlockBounds(0.0, 0.0, 0.0, 0.5, 1.0, 1.0);
+				break;
+			case EAST:
+				this.setBlockBounds(0.5, 0.0, 0.0, 1.0, 1.0, 1.0);
+				break;
 		}
-		if (axis == Axis.Z) {
-			meta += 3;
+	}
+
+	@Override
+	public AABB getCollisionBoundingBoxFromPool(WorldSource world, int x, int y, int z) {
+		int meta = world.getBlockMetadata(x, y, z);
+		boolean isDoubled = (meta & 0b1000) != 0;
+		if (isDoubled){
+			return AABB.getBoundingBoxFromPool(x, y, z, x + 1.0f, y + 1.0f, z + 1.0f);
 		}
-		world.setBlockMetadataWithNotify(x, y, z, meta);
+
+		Side side = Side.getSideById(meta & 0b111);
+		switch (side){
+			case BOTTOM:
+				return AABB.getBoundingBoxFromPool(x, y, z, x + 1.0f, y + 0.5f, z + 1.0f);
+			case TOP:
+				return AABB.getBoundingBoxFromPool(x, y + 0.5f, z, x + 1.0f, y + 1.0f, z + 1.0f);
+			case NORTH:
+				return AABB.getBoundingBoxFromPool(x, y, z, x + 1.0f, y + 1f, z + 0.5f);
+			case SOUTH:
+				return AABB.getBoundingBoxFromPool(x, y, z + 0.5f, x + 1.0f, y + 1.0f, z + 1.0f);
+			case WEST:
+				return AABB.getBoundingBoxFromPool(x, y, z, x + 0.5f, y + 1.0f, z + 1.0f);
+			case EAST:
+				return AABB.getBoundingBoxFromPool(x + 0.5f, y, z, x + 1.0f, y + 1.0f, z + 1.0f);
+		}
+		return AABB.getBoundingBoxFromPool(x, y, z, x + 1.0f, y + 1.0f, z + 1.0f);
 	}
 }
--- src/main/java/mayonaka8478/bambooremake/block/SlabTatamiMetaStateInterpreter.java
+++ src/main/java/mayonaka8478/bambooremake/block/SlabTatamiMetaStateInterpreter.java
@@ -1,20 +1,26 @@
 package mayonaka8478.bambooremake.block;
 
 import net.minecraft.core.block.Block;
+import net.minecraft.core.util.helper.Axis;
+import net.minecraft.core.util.helper.Side;
 import net.minecraft.core.world.WorldSource;
 import org.useless.dragonfly.model.blockstates.processed.MetaStateInterpreter;
 
 import java.util.HashMap;
+import java.util.Locale;
 
 public class SlabTatamiMetaStateInterpreter extends MetaStateInterpreter {
 
 	@Override
 	public HashMap<String, String> getStateMap(WorldSource worldSource, int x, int y, int z, Block block, int meta) {
-		int hRotation = meta & 2;
-		int vRotation = meta & 5;
+		Side side = Side.getSideById(meta & 0b111); // Get the stored axis of the slab
+		boolean isDoubled = (meta & 0b1000) != 0;
+
 		HashMap<String, String> result = new HashMap<>();
-		result.put("half", new String[]{"bottom", "double", "top"}[hRotation]);
-		result.put("facing", vRotation == 0 ? "x" : "z");
+		result.put("doubled",isDoubled ? "true" : "false");
+		result.put("facing", side.name().toLowerCase(Locale.ROOT));
+
+		System.out.println(result);
 		return result;
 	}
 }
